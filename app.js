6a51909979bc620a0aa812a75bc7007aa121a7ea
quangta93 <quang.tran@utdallas.edu>
2015-02-08 10:51:32 -0600

    dev

--- app.js
+++ app.js
@@ -256,7 +256,7 @@ app.controller('StudentCtrl', ['$scope', '$rootScope', 'USER', function($scope, 
 
         }
       } // END loop
-    }, function() { alert("error loading message"); });
+    }, function() { console.debug("error loading message"); });
   }
   
   $(document).ready(function() {
@@ -269,15 +269,186 @@ app.controller('StudentCtrl', ['$scope', '$rootScope', 'USER', function($scope, 
     editor.getSession().setMode("ace/mode/javascript");
     editor.setFontSize(13);
     editor.$blockScrolling = Infinity;
+    $scope.editor = editor;
+
+
+// global variables
+var isLoggedIn = false;
+var intervalRef = null;
+
+/**************** TEACHER'S LISTENERS ****************/
+
+var onData = function(data) {
+    console.debug('data' + JSON.stringify(data))
+    $scope.editor.getSession().setValue(data.payload)
+    // editor.setValue(data.payload)
+}
+var onSessionUserJoinRequest = function(notification) {
+  console.debug('received join request: ' + JSON.stringify(notification))
+  // approve user request
+  KandyAPI.Session.acceptJoinRequest(notification.session_id, notification.full_user_id,
+        function () {
+            console.log('teacher approve request');
+        },
+        function (msg, code) {
+            showStatusMessage("Attempt to accept user's join request failed (" + code + "): " + msg);
+        }
+  );
+}
+var onUserJoin = function() {
+  // start sending data
+  console.debug('student joined');
+  toast('Student is online.', 4000);
+  intervalRef = setInterval(prepareAndSend, 1000);
+}
+var prepareAndSend = function() {
+  var data = $scope.editor.getSession().getValue()
+  // console.debug($scope.editor.sesison.getDocument())
+  KandyAPI.Session.sendData(SESSION_ID, data);
+}
+var onUserLeave = function() {
+  toast('Student has left.', 4000);
+  if (intervalRef != null) clearInterval(intervalRef);
+}
+
+var teacherListeners = {
+  'onData': onData,
+    'onUserJoinRequest': onSessionUserJoinRequest,
+    'onUserJoin': onUserJoin,
+    'onUserLeave': onUserLeave
+};
+
+/**************** STUDENT'S LISTENERS ****************/
+
+var onActive = function() {
+  toast('Teacher is online.', 4000);
+  var joinConfig = {
+            user_nickname: STUDENT_CALL_USER,
+            user_first_name: null,
+            user_last_name: null,
+            user_phone_number: null,
+            user_email: null
+        };
+  KandyAPI.Session.join(SESSION_ID, joinConfig, onApproved, failure(msg, code));
+}
+var onInactive = function() {
+  toast('Something went wrong! Session is inactivated.', 4000);
+}
+var onApproved = function() {
+  console.debug('student joined session');
+  intervalRef = setInterval(prepareAndSend, 1000);
+}
+
+var studentListeners = {
+  'onData': onData,
+  'onActive': onActive,
+  'onInactive': onInactive,
+  'onJoinApproved': onApproved,
+};
+
+/********************** SESSION **********************/
+
+var setupSession = function(type, editor) {
+  console.debug('setup ' + type)
+  // type=[TEACHER_CALL_USER, STUDENT_CALL_USER]
+  if (!isLoggedIn) {
+    KandyAPI.login(APIKEY, type + '@' + DOMAIN_NAME, PASSWORD,
+      function(results) {
+        console.debug(type + " logged in.");
+        isLoggedIn = true;
+
+      if (type == TEACHER_CALL_USER) {
+        console.debug('retrieveSession');
+        retrieveSession();
+      } else if (type == STUDENT_CALL_USER) {
+        console.debug('joinSession');
+        joinSession();
+      }
+      }, failure(type + ' failed to login', 500)
+    );
+  }
+}
+
+// Function called by teacher view only
+// Retrieve default session and start sending data
+var retrieveSession = function() {
+  // set teacher's listener
+  KandyAPI.Session.getInfoById(SESSION_ID, 
+    function(results) {
+      // on success, activate
+      if (results.session.session_status=='inactive') {
+        KandyAPI.Session.activate(SESSION_ID, 
+          function(res) {
+            console.debug('teacher activated. listeners=' + JSON.stringify(teacherListeners));
+            KandyAPI.Session.setListeners(SESSION_ID, teacherListeners);
+          },
+          function() {
+            if (msg != undefined) failure(msg, code)
+          }
+        );
+      } else {
+        console.debug('activated. listeners=' + JSON.stringify(teacherListeners));
+        KandyAPI.Session.setListeners(SESSION_ID, teacherListeners);
+      }
+    }, failure('', 500)
+  );
+}
+
+// Function called by student view only
+// Check availability and start sending data
+var joinSession = function() {
+  // Get session to join
+  KandyAPI.Session.getInfoById(SESSION_ID,
+    function(res) {
+      console.debug('student got session info')
+      if (res.session.session_status=='active') {
+        KandyAPI.Session.setListeners(SESSION_ID, studentListeners);  
+          var joinConfig = {};
+          KandyAPI.Session.join(SESSION_ID, joinConfig,
+            function() {
+              console.debug('student joined')
+            }, 
+            function() {
+              if (msg != undefined) failure(msg, code)
+            }
+          );
+      }
+    }, failure('cannot get info by id', 500)
+  );
+}
+
+var setLogoutOnUnload = function(type) {
+  if (type == TEACHER_CALL_USER) {
+    // KandyAPI.Session.inactivate(SESSION_ID, function() {console.debug('session activated')}, failure(msg, code));
+    KandyAPI.Session.leave(SESSION_ID, 'reason to leave', function() { console.debug('teacher left session')}, failure(msg, code))
+    clearInterval(intervalRef)
+    isLoggedIn = false;
+  } else if (type == STUDENT_CALL_USER) {
+    // close video call?
+    // leave session
+    KandyAPI.Session.leave(SESSION_ID, 'reason to leave', function(){console.debug('student left session')}, failure(msg, code));
+    clearInterval(intervalRef)
+    // logout
+    isLoggedIn = false;
+  }
+}
+
+var failure = function(msg, code) {
+  console.debug("err : msg=" + msg + "; status=" + status);
+  console.debug(isLoggedIn)
+}
+
+
 
     // setup kandy video call
     setLogoutOnUnload();
-    setup();
-    login($scope.ME);
+    // setup();
+    // login($scope.ME);
+    setupSession($scope.ME);
     if ($scope.presenceWatch !== undefined) {
       KandyAPI.Phone.watchPresence($scope.presenceWatch);
     }
-    setInterval(getMsg, 3000);
+    setInterval(getMsg, 5000);
 
     // setup kandy cobrowsing
     // loginUser();
--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var TEACHER_COBROWSE_USER = 'user3';
 var STUDENT_COBROWSE_USER = 'user4';
 
 var SESSION = '{"session_id":"52007725c35f41068cac4fc8db4f2348","session_type":"support","session_status":"inactive","creation_timestamp":1423365412828,"expiry_timestamp":1423387012827,"admin_full_user_id":"user3@mahindra15.com","domain_name":"mahindra15.com"}';
-var SESSION_ID = '52007725c35f41068cac4fc8db4f2348';
+var SESSION_ID = '9d3bffc30e1d46bfbd5c8ea62d0f90c2';
 var DOMAIN_NAME = 'mahindra15.com';
 
 var STUDENT_PHONE_NUMBER = '+1' + '6825219682';
